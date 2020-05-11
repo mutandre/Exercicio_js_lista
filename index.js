51726723eb55d890cd6b4a449b08658c82a7450b
Mutu Andre <mutuandre@gmail.com>
2020-05-11 16:49:58 -0300

    Inserção da condição if

--- index.js
+++ index.js
@@ -1,23 +1,35 @@
 
 function adicionar() {
 
-    /** Atribuição de outras variavéis  */
+      /** Atribuição de outras variavéis  */
 
-    var nome = document.querySelector("#inserido").value;
-    var entrada = document.querySelector("#list");
+      var nome = document.querySelector("#inserido").value;
 
-    /* Criar novos itens tanto li quanto texto nele será inserido*/
-    var linovo = document.createElement("li");
-    var txtnovo = document.createTextNode(nome);
-
-
-    linovo.appendChild(txtnovo);
-    entrada.appendChild(linovo);
-
-    /* Aplicando espaçamento no novo item adicionado na lista */
-    linovo.style.padding = "0.5rem 0 0.5rem 1rem";
-    linovo.style.margin = "margin: 0.1rem 0";
-    linovo.style.borderBottom = "1px solid #B2DBBF";
+      if (nome == null || nome==""){
+  
+          alert("Por favor, o campo precisa de informação!");
+          document.querySelector("#inserido").focus();
+          return false;
+      }
+      else {
+  
+          var entrada = document.querySelector("#list");
+          
+          /* Criar novos itens tanto li quanto texto nele será inserido*/
+          var linovo = document.createElement("li");
+          var txtnovo = document.createTextNode(nome);
+  
+  
+          linovo.appendChild(txtnovo);
+          entrada.appendChild(linovo);
+  
+          /* Aplicando espaçamento no novo item adicionado na lista */
+          linovo.style.padding = "0.5rem 0 0.5rem 1rem";
+          linovo.style.margin = "margin: 0.1rem 0";
+          linovo.style.borderBottom = "1px solid #B2DBBF";
+  
+   
+      }
 
 
 }
