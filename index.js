98c2ab4c39f32f36e2259fd5800b085c4a3a8aad
Mutuyekiani João André <61994669+mutandre@users.noreply.github.com>
2020-05-11 13:47:42 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,23 @@
+
+function adicionar() {
+
+    /** Atribuição de outras variavéis  */
+
+    var nome = document.querySelector("#inserido").value;
+    var entrada = document.querySelector("#list");
+
+    /* Criar novos itens tanto li quanto texto nele será inserido*/
+    var linovo = document.createElement("li");
+    var txtnovo = document.createTextNode(nome);
+
+
+    linovo.appendChild(txtnovo);
+    entrada.appendChild(linovo);
+
+    /* Aplicando espaçamento no novo item adicionado na lista */
+    linovo.style.padding = "0.5rem 0 0.5rem 1rem";
+    linovo.style.margin = "margin: 0.1rem 0";
+    linovo.style.borderBottom = "1px solid #B2DBBF";
+
+
+}
