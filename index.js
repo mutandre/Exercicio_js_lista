f412fae14d4fb1b8191cc3a8f962b35517ff1964
Mutu Andre <mutuandre@gmail.com>
2020-05-13 18:06:12 -0300

    Inserção de botão

--- index.js
+++ index.js
@@ -1,34 +1,61 @@
+/*const btn_texto= document.querySelector(".task-list__action")
+ 
+function alt (ligando){
+
+    if (ligando==true){
+      linovo.classList.add("task-list__item");
+    }
+    else{
+      linovo.classList.add ("task-list__item--done")
+    }
+      
+
+}
+*/
+
+
 
 function adicionar() {
 
       /** Atribuição de outras variavéis  */
-
+    
       var nome = document.querySelector("#inserido").value;
-
       if (nome == null || nome==""){
   
-          alert("Por favor, o campo precisa de informação!");
+         /* alert("Por favor, o campo precisa de informação!");*/
           document.querySelector("#inserido").focus();
           return false;
       }
       else {
   
-          var entrada = document.querySelector("#list");
-          
-          /* Criar novos itens tanto li quanto texto nele será inserido*/
-          var linovo = document.createElement("li");
-          var txtnovo = document.createTextNode(nome);
+        var entrada = document.querySelector("#list");
+      
+      /* Criação de botões e inserção de texto */ 
+        const btn = document.createElement("BUTTON");
+        const t = document.createTextNode("..");
+
+        /* Criar novos itens tanto li quanto texto nele será inserido*/
+        var linovo = document.createElement("li");
+        var txtnovo = document.createTextNode(nome);
   
   
-          linovo.appendChild(txtnovo);
-          entrada.appendChild(linovo);
+        document.body.appendChild(btn);
+        btn.appendChild(t);
+
+        linovo.appendChild(txtnovo);
+        linovo.appendChild(btn);
+
+        entrada.appendChild(linovo);
   
-          /* Aplicando espaçamento no novo item adicionado na lista */
-          linovo.style.padding = "0.5rem 0 0.5rem 1rem";
-          linovo.style.margin = "margin: 0.1rem 0";
-          linovo.style.borderBottom = "1px solid #B2DBBF";
-  
-   
+        /* Aplicando espaçamento no novo item adicionado na lista */
+ 
+        linovo.classList.add("task-list__item");
+        btn.classList.add("task-list__action")
+         
+        /* Limpar campo e inserir o focus novamente*/
+        document.querySelector("#inserido").value="";
+        document.querySelector("#inserido").focus();
+
       }
 
 
